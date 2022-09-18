3e4aa67cc75e95662e8171f118d0b5613ce41594
mateusdegobi <kim3dis@gmail.com>
2022-09-18 11:10:59 -0300

    :construction: add: pressable button

--- src/components/RadioBox/styled.ts
+++ src/components/RadioBox/styled.ts
@@ -2,12 +2,13 @@ import styled from 'styled-components/native';
 import Text from '../Text';
 import {RFValue} from 'react-native-responsive-fontsize';
 
-export const Background = styled.View`
+export const Background = styled.TouchableOpacity`
   background: #d9d9d9;
   border-radius: 15px;
   padding: 15px 15px;
   flex-direction: row;
   width: 100%;
+  margin-top: 15px;
 `;
 export const TextSide = styled.View`
   width: 90%;
--- src/pages/Report/Report.tsx
+++ src/pages/Report/Report.tsx
@@ -2,12 +2,17 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import TitleLarge from '../../components/TitleLarge';
 import RadioBox from '../../components/RadioBox';
+import {useNavigation} from '@react-navigation/native';
 
 export default function Report() {
+  const nav = useNavigation();
+
   return (
     <View style={{backgroundColor: 'white', flex: 1, padding: 20}}>
       <TitleLarge>Talvez você queira...</TitleLarge>
-      <RadioBox description="Como tapa-buracos, coleta de entulhos, etc">
+      <RadioBox
+        description="Como tapa-buracos, coleta de entulhos, etc"
+        onPress={() => nav.navigate('ReportFormScene1')}>
         Deixar sugestões ou pedidos
       </RadioBox>
       <RadioBox description="Como tapa-buracos, coleta de entulhos, etc">
